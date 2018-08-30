b3673744628c48350ad2f3621b44ae26f877be25
Timothy Harley <timothy.d.harley@gmail.com>
2018-08-29 20:53:24 -0500

    css stuff

--- project.js
+++ project.js
@@ -1,13 +1,32 @@
 let projects = [
-    {
+    {   
         title: "Cool Project", 
-        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        screenshot: "<img src='http://gotoflashgames.com/files/file/033.jpg'>", 
         description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
         technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
         available: true,
-        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
-      }
+        url: "<a href='https://github.com/nss-evening-cohort-8/js-part-deux'>URL</a>", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "<a href='https://github.com/nss-evening-cohort-8/js-part-deux'>Github URL</a>"
+    },
+
+    {   
+        title: "Cool Project", 
+        screenshot: "<img src='http://gotoflashgames.com/files/file/033.jpg'>", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: false,
+        url: "<a href='https://github.com/nss-evening-cohort-8/js-part-deux'>URL</a>", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "<a href='https://github.com/nss-evening-cohort-8/js-part-deux'>Github URL</a>"
+    },  
+    {   
+        title: "Cool Project", 
+        screenshot: "<img src='http://gotoflashgames.com/files/file/033.jpg'>", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: true,
+        url: "<a href='https://github.com/nss-evening-cohort-8/js-part-deux'>URL</a>", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "<a href='https://github.com/nss-evening-cohort-8/js-part-deux'>Github URL</a>"
+    },
 ];
 
 
@@ -19,18 +38,20 @@ const writeToDom = (stringToPrint,divId) => {
 const createProjectCards = () => {
     let newString = '';
     for(let i=0; i<projects.length; i++){
+        if (projects[i].available === true) {
          newString += `<div id="projectsPage">`
          newString +=`<h3>${projects[i].title}<h3>`
          newString +=`<h3>${projects[i].screenshot}<h3>`
          newString +=`<h3>${projects[i].description}<h3>`
          newString +=`<h3>${projects[i].technologiesUsed}<h3>`
-         newString +=`<h3>${projects[i].available}<h3>`
          newString +=`<h3>${projects[i].url}<h3>`
          newString +=`<h3>${projects[i].githubUrl}<h3>`
          newString +=`</div>`;
-     
+        
     }
+    
      writeToDom(newString, 'projectsPage');
+  }
 };
 
 createProjectCards();
