a7aaa11b662fe629e2c45a995e4c888002d70ccc
Timothy Harley <timothy.d.harley@gmail.com>
2018-11-05 21:30:35 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import $ from 'jquery';
-
 import 'bootstrap';
-
 import './index.scss';
+import axios from 'axios';
 
-// import axios from 'axios';
+import mountains from './images/mountains.png';
 
+$('#mountains').attr('src', mountains);
 
 // NavBar Variables
 // const toBio = document.getElementById('navToBio');
-const toTech = document.getElementById('navToTechnologies');
-const toProjects = document.getElementById('navToProjects');
 const bioPage = document.getElementById('bioPage');
 const technologiesPage = document.getElementById('technologiesPage');
 const projectsPage = document.getElementById('projectsPage');
@@ -22,51 +20,51 @@ $('#navToBio').on('click', () => {
   projectsPage.style.display = 'none';
 });
 
-toTech.addEventListener('click', () => {
+$('#navToTechnologies').on('click', () => {
   bioPage.style.display = 'none';
   technologiesPage.style.display = 'block';
   projectsPage.style.display = 'none';
 });
 
-toProjects.addEventListener('click', () => {
+$('#navToProjects').on('click', () => {
   bioPage.style.display = 'none';
   technologiesPage.style.display = 'none';
   projectsPage.style.display = 'block';
 });
 
-const getProjects = () => new Promise((resolve, reject) => {
-  $.get('http://localhost:3004/projects')
-    .done((data) => {
-      console.log(data);
-      resolve(data);
-    })
-    .fail((error) => {
-      reject(error);
-    });
-});
+const getProjects = () => axios.get('http://localhost:3004/projects');
 
-
-const createProjectCards = (arrayOfProjects) => {
+const createProjectCards = (projects) => {
   let newString = '';
-  arrayOfProjects.forEach((project) => {
+  projects.forEach((project) => {
     if (project.available === true) {
-      newString += '<div id="projectsPage">';
-      newString += `<h3>${project.title}<h3>`;
-      newString += `<h3>${project.screenshot}<h3>`;
-      newString += `<h3>${project.description}<h3>`;
-      newString += `<h3>${project.technologiesUsed}<h3>`;
-      newString += `<h3>${project.url}<h3>`;
-      newString += `<h3>${project.githubUrl}<h3>`;
-      newString += '</div>';
+      newString += `
+    <div class="d-flex justify-content-center">  
+      <div class="card" style="width: 40rem;">
+        <img class="card-img-top" src="${project.screenshot}" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${project.title}</h5>
+          <p class="card-text">${project.description}</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Technologies Used:</li>
+          <li class="list-group-item">${project.technologiesUsed}</li>
+        </ul>
+        <div class="card-body">
+          <a href="#${project.url}" class="card-link">Project link</a>
+          <a href="#${project.github}" class="card-link">Github Link</a>
+        </div>
+      </div>
+    </div>`;
     }
   });
   $('#projectsPage').html(newString);
 };
 
-const initProjects = () => {
-  getProjects().then((data) => {
-    createProjectCards(data);
+getProjects()
+  .then((data) => {
+    createProjectCards(data.data);
+  })
+  .catch((error) => {
+    console.error({ error });
   });
-};
-
-initProjects();
