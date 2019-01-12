ad273b728cbae49534c04cad53e66e180f82f77a
Timothy Harley <timothy.d.harley@gmail.com>
2019-01-12 00:31:43 -0600

    favicon and misspellings

--- src/components/bioPage/bioPage.js
+++ src/components/bioPage/bioPage.js
@@ -10,9 +10,9 @@ const createBioPage = () => {
   <div class="col-6">
     <p class="indented">For the past decade, I have been a cook at Nashville's famous Donut Den.  The best part of that job is that at the end of the day, I can step back and admire the work I've finished.  The worst part of that job is that I've peaked.  
     There wasn't anything left for me to learn.  I learned a little coding recreationly and quickly found that I enjoyed it.  Just like 
-    cooking I could admire my finished product.  Unlike working in a bakery, however, I could continue to learn and discover new things indefinitly in software coding.  I decided it was time for a career change. </p>
+    cooking I could admire my finished product.  Unlike working in a bakery, however, I could continue to learn and discover new things indefinitely in software coding.  I decided it was time for a career change. </p>
     <br/>
-    <p class="indented">I like playing videogames, solving puzzles, watching documentaries, and spending time with my family and my dogs, Logan and Nora.  When I have time, I love to travel, go backpacking, and explore new places.  
+    <p class="indented">I like playing videogames, solving puzzles, watching documentaries, and spending time with my wife, Rachel, and my dogs, Logan and Nora.  When I have time, I love to travel, go backpacking, and explore new places.  
     I grew up with a family that encouraged me to discover the answers to all my questions rather than expect them to be handed to me.  As a result, I can be a little stubborn about asking for help when I feel like I can figure something out on my own.
     However, at Nashville Software School, I'm learning to be better about asking for help asking when I'm stuck.</p>
   </div>
