911a481296bb9e359e9a927f61dfb3a93198a71a
Timothy Harley <timothy.d.harley@gmail.com>
2019-01-12 02:40:37 -0600

    parallax added for first two layers

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import 'bootstrap';
 import './index.scss';
 
 import navbar from './components/navbar/navbar';
-import mountains from './images/mountains.png';
+import parallax1 from './images/parallax/Background1.png';
+import parallax2 from './images/parallax/Background2.png';
 import bioPage from './components/bioPage/bioPage';
 import techPage from './components/techPage/techPage';
 import projectsPage from './components/projectsPage/projectPage';
 import footer from './components/footer/footer';
 
-$('#mountains').attr('src', mountains);
+$('#parallax1').attr('src', parallax1);
+$('#parallax2').attr('src', parallax2);
+
 
 const initializeApp = () => {
   navbar();
