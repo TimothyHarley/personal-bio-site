042d6ea8cd5abbd9a2932b228877cc634c34d700
Timothy Harley <timothy.d.harley@gmail.com>
2019-01-12 00:51:34 -0600

    small correction

--- src/components/bioPage/bioPage.js
+++ src/components/bioPage/bioPage.js
@@ -12,7 +12,7 @@ const createBioPage = () => {
     There wasn't anything left for me to learn.  I learned a little coding recreationly and quickly found that I enjoyed it.  Just like 
     cooking I could admire my finished product.  Unlike working in a bakery, however, I could continue to learn and discover new things indefinitely in software coding.  I decided it was time for a career change. </p>
     <br/>
-    <p class="indented">I like playing videogames, solving puzzles, watching documentaries, and spending time with my wife, Rachel, and my dogs, Logan and Nora.  When I have time, I love to travel, go backpacking, and explore new places.  
+    <p class="indented">I like playing videogames, solving puzzles, watching documentaries, and spending time with my beautiful wife, Rachel, and my dogs, Logan and Nora.  When I have time, I love to travel, go backpacking, and explore new places.  
     I grew up with a family that encouraged me to discover the answers to all my questions rather than expect them to be handed to me.  As a result, I can be a little stubborn about asking for help when I feel like I can figure something out on my own.
     However, at Nashville Software School, I'm learning to be better about asking for help asking when I'm stuck.</p>
   </div>
