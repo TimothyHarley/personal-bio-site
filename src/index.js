3bedecddc908d4960e1a6c26f0051612579235af
Timothy Harley <timothy.d.harley@gmail.com>
2019-01-12 04:30:07 -0600

    all 5 layers parallaxed

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import './index.scss';
 import navbar from './components/navbar/navbar';
 import parallax1 from './images/parallax/Background1.png';
 import parallax2 from './images/parallax/Background2.png';
+import parallax3 from './images/parallax/Background3.png';
+import parallax4 from './images/parallax/Background4.png';
+import parallax5 from './images/parallax/Background5.png';
 import bioPage from './components/bioPage/bioPage';
 import techPage from './components/techPage/techPage';
 import projectsPage from './components/projectsPage/projectPage';
@@ -12,7 +15,9 @@ import footer from './components/footer/footer';
 
 $('#parallax1').attr('src', parallax1);
 $('#parallax2').attr('src', parallax2);
-
+$('#parallax3').attr('src', parallax3);
+$('#parallax4').attr('src', parallax4);
+$('#parallax5').attr('src', parallax5);
 
 const initializeApp = () => {
   navbar();
