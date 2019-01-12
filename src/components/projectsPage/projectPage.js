c8c3e95ced8493430854fc4ce2d40c0a46938385
Timothy Harley <timothy.d.harley@gmail.com>
2019-01-11 23:44:03 -0600

    working on projectPage scss

--- src/components/projectsPage/projectPage.js
+++ src/components/projectsPage/projectPage.js
@@ -3,37 +3,40 @@ import './projectPage.scss';
 import getProjects from '../../helpers/projectRequest';
 
 const createProjectsPage = (projects) => {
-  let domString = '';
+  let domString = '<div class="d-flex flex-row flex-wrap mx-auto" style="width: 80%">';
 
   projects.forEach((project) => {
     if (project.available === true) {
       domString += `
-    <div class="d-flex justify-content-center">  
-      <div class="card" style="width: 40rem;">
+    <div class="extraDiv">  
+      <div class="card">
         <img class="card-img-top" src="${project.screenshot}" alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">${project.title}</h5>
           <p class="card-text">${project.description}</p>
         </div>
         <ul class="list-group list-group-flush">
-          <li class="list-group-item">Technologies Used:</li>
-          <li class="list-group-item">${project.technologiesUsed}</li>
+          <li class="list-group-item">
+            <div>Technologies Used:</div>
+            ${project.technologiesUsed}
+          </li>
         </ul>
         <div class="card-body">
-          <a href="#${project.url}" class="card-link">Project link</a>
-          <a href="#${project.github}" class="card-link">Github Link</a>
+          <a href="${project.url}" class="card-link">Project link</a>
+          <a href="${project.githubUrl}" class="card-link">Github Link</a>
         </div>
       </div>
     </div>`;
     }
   });
 
+  domString += '</div>';
+
   $('#projectsPage').html(domString);
 };
 
 getProjects()
   .then((data) => {
-    console.log(data);
     createProjectsPage(data);
   })
   .catch((error) => {
